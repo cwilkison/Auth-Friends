31bc1b4a1938c8f479914c27904ffcc23d16e765
Cole Wilkison <wilkison.cole@gmail.com>
2020-04-21 21:39:03 -0400

    only need to add friend function

--- friends/src/App.js
+++ friends/src/App.js
@@ -2,26 +2,26 @@ import React from 'react';
 import './App.css';
 import Login from './components/Login';
 import { BrowserRouter as Router, Link, Route, Switch } from 'react-router-dom';
-import PrivateRoute from './components/ProtectedRoute';
+import ProtectedRoute from './components/ProtectedRoute';
 import FriendsList from './components/FriendsList';
+
 
 function App() {
   return (
     <Router>
-      <div className="App">
-        <nav>
-            <Link to="/login">Login</Link>
-            <br />
-            <Link to="/protected">Protected Page</Link>
-        </nav>
-        <Switch>
-          <PrivateRoute exact path="/protected" component={FriendsList} />
-          <Route path="/login" component={Login} />
-          <Route component={Login} />
-        </Switch>
-      </div>
+    <div className="App">
+      <header>
+      <Link to="/login">Login</Link>
+         <Link to="/protected">Protected Page</Link>
+
+          <Switch>
+            <ProtectedRoute exact path ='/protected' component= {FriendsList}/>
+            <Route path='/login' component ={Login} />
+            <Route component={Login} />
+          </Switch>
+      </header>
+    </div>
     </Router>
   );
-}
-
-export default App;
+  }
+  export default App;
--- friends/src/components/ProtectedRoute.js
+++ friends/src/components/ProtectedRoute.js
@@ -1,18 +1,17 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
-
-const PrivateRoute = ({ component: Component, ...rest }) => {
-  return (
-    <Route
-      {...rest}
-      render={props => {
-        if (localStorage.getItem('token')) {
-          return <Component {...props} />; // render component passed into props
-        } else {
-          return <Redirect to="/login" />;
-        }
-      }}
-    />
-  );
-};
-export default PrivateRoute;
+ const ProtectedRoute = ({ component: Component, ...rest }) => {
+     return (
+        <Route
+        {...rest}
+        render={() => {
+          if (localStorage.getItem("token")) {
+            return <Component />;
+          } else {
+            return <Redirect to="/login" />;
+          }
+        }}
+      />
+     );
+ };
+ export default ProtectedRoute;
